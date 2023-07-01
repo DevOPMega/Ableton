4bde6fa5408d07619638f543f6c85bccbd9e58f2
Adesh Singh <89608254+DevOPMega@users.noreply.github.com>
2023-07-01 12:52:23 +0530

    Delete Ableton directory

--- Ableton/index.js
+++
@@ -1,7 +0,0 @@
-const menuBtn = document.getElementsByClassName("menu-btn")[0];
-const menu = document.getElementsByClassName("menu")[0];
-console.log(menuBtn, menu);
-
-menuBtn.addEventListener("click", ()=>{
-    menu.classList.toggle("active-menu");
-})
